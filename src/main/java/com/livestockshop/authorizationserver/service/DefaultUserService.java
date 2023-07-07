e249d40d7157bbbbfc5428ae2bb76efe3c32be6e
Luke Sukhanov <lukesukhanov@gmail.com>
2023-07-07 03:04:15 +0300

    Remove redundant scopes for new users

--- src/main/java/com/livestockshop/authorizationserver/service/DefaultUserService.java
+++ src/main/java/com/livestockshop/authorizationserver/service/DefaultUserService.java
@@ -39,10 +39,7 @@ public class DefaultUserService implements UserService {
     UserEntity userEntity = new UserEntity();
     userEntity.setEmail(email);
     userEntity.setPassword(this.passwordEncoder.encode(password));
-    userEntity.setAuthorities(Set.of(
-        new SimpleGrantedAuthority("ROLE_USER"),
-        new SimpleGrantedAuthority("openid"),
-        new SimpleGrantedAuthority("profile")));
+    userEntity.setAuthorities(Set.of(new SimpleGrantedAuthority("ROLE_USER")));
     userEntity = this.userRepository.save(userEntity);
     return userEntity.getId();
   }
